b2a04389f2773f1fd305a2588d9ad2609c368181
shazidhasanriam <shazid.riam@northsouth.edu>
2021-11-24 23:52:50 +0600

    Initial Final Update

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 50;
 const apiKey = "cskK-RyjpgwJot_kfg8U4VvHWGbTUVTqAvpBtZ8ZH00";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Image loading check
+function imageLoaded() {
+  //   console.log("Image Loaded");
+  imagesLoaded++;
+  console.log(imagesLoaded);
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log("ready =", ready);
+  }
+}
+
+// Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 // Creating elements for links & Photos and Adding to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("Total Images: ", totalImages);
   // Run function for each object in Photos array
   photosArray.forEach((photo) => {
     // Creating <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
-
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // Creating <img> for photos
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
+    // Event Listener
+    img.addEventListener("load", imageLoaded);
     // Putting <img> inside <a>, then put both inside imageContainer
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -39,6 +74,19 @@ async function getPhotos() {
     // Catch errors here
   }
 }
+
+// Load more photos
+window.addEventListener("scroll", () => {
+  //   console.log("Scrolled");
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+    // console.log("Load more photos");
+  }
+});
 
 // On Load
 getPhotos();
